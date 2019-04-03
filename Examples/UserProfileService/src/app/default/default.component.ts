896f27ef5e1bb36013bf739b63bc44ba3c7d44d4
Matt Wilson <matwils@microsoft.com>
2019-04-03 12:06:58 -0700

    Building out the user profile service example.

---
+++ Examples/UserProfileService/src/app/default/default.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'default-component',
+  templateUrl: './default.component.html',
+  styleUrls: ['./default.component.css']
+})
+export class DefaultComponent implements OnInit {
+  constructor() {
+    //
+  }
+
+  public ngOnInit() {
+    //
+  }
+
+}
