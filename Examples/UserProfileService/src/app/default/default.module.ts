793b49cb7015e9ac8ec9f68025afc9a70650ddbb
Matt Wilson <matwils@microsoft.com>
2019-04-04 16:47:42 -0700

    Finishing up the profile example.

--- Examples/UserProfileService/src/app/default/default.module.ts
+++ Examples/UserProfileService/src/app/default/default.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LoadingWheelModule } from '@microsoft/windows-admin-center-sdk/angular';
 
 import { DefaultComponent } from './default.component';
 import { Routing } from './default.routing';
@@ -7,7 +9,9 @@ import { Routing } from './default.routing';
 @NgModule({
   imports: [
     CommonModule,
-    Routing
+    Routing,
+    LoadingWheelModule,
+    FormsModule
   ],
   declarations: [DefaultComponent]
 })
